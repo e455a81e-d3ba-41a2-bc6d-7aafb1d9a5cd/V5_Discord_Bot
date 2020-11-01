8fbeb69008f8c2587d97c89203151429ea25c35f
leonhardt <lariscusObscurus@gmail.com>
2020-11-01 02:39:32 +0100

    License

--- V5_Discord_Bot/Services/DiceRollerService.cs
+++ V5_Discord_Bot/Services/DiceRollerService.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+* Copyright 2019 Leonhardt Schwarz
+*
+* Licensed under the EUPL, Version 1.2 only (the "Licence");
+* You may not use this work except in compliance with the
+Licence.
+* You may obtain a copy of the Licence at:
+*
+* https://joinup.ec.europa.eu/software/page/eupl 5
+*
+* Unless required by applicable law or agreed to in
+writing, software distributed under the Licence is
+distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+* See the Licence for the specific language governing
+permissions and limitations under the Licence.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using V5_Discord_Bot.Model;
